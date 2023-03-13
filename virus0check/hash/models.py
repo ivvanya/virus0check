252cd33a1012ecce12e84259c720be7d9ae15acf
spagj <dunkstrong@gmail.com>
2023-03-13 19:19:08 +0700

    start fileupload

--- virus0check/hash/models.py
+++ virus0check/hash/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.TextField()
+    cover = models.FileField(upload_to='files/')
+
+    def __str__(self):
+        return self.title
