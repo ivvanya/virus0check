b6ec1056c9a12cf19eb878d295a41a11d3b72280
Ivan <vanya.avdyukov@yandex.ru>
2023-03-15 16:38:30 +0700

    спаси и сохрани js

--- virus0check/hash/static/js/script.js
+++ virus0check/hash/static/js/script.js
@@ -1,6 +1,1 @@
-jQuery( "input.wpcf7-form-control.wpcf7-submit" ).click(function() {   
-        var link = document.createElement('a');
-        link.setAttribute('href','http://billgroup.kg/wp-content/uploads/pdf/DeluxeAntalya-Presentation.pdf');
-        link.setAttribute('download','download');
-        link.click();
-    }
+console.log('123')
